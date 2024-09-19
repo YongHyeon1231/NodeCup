18e40648412947a1fddbf1bb885f3dc33a45e773
applaud13 <applaud13@gmail.com>
2024-09-19 14:49:11 +0900

    api구현

--- src/app.js
+++ src/app.js
@@ -1,8 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
-
-import usersRouter from './routes/users.route.js';
-
+import shopRouter from './routes/shop.route.js';
+import upgradeRouter from './routes/upgrade.route.js';
 import ErrorHandlingMiddleware from './middlewares/error-handling.middleware.js';
 
 // .env 파일을 읽어서 process.env에 추가합니다.
@@ -13,10 +12,10 @@ const PORT = process.env.DATABASE_PORT;
 
 app.use(express.json()); // body parser 역할
 
-app.use('/api', [usersRouter]);
+app.use('/api', [shopRouter, upgradeRouter]);
 
 app.use(ErrorHandlingMiddleware);
 
 app.listen(PORT, () => {
-    console.log(PORT, '포트로 서버가 열렸습니다.');
+  console.log(PORT, '포트로 서버가 열렸습니다.');
 });
