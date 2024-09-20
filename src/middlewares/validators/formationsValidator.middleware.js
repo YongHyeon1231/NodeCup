74cb627495b35da4701f6022d74690fa6d6b4f99
YongHyeon1231 <120100530+YongHyeon1231@users.noreply.github.com>
2024-09-20 17:35:27 +0900

    Main.ver.0.1.2

--- src/middlewares/validators/formationsValidator.middleware.js
+++ src/middlewares/validators/formationsValidator.middleware.js
@@ -1,36 +1,35 @@
-import Joi from "joi";
+import Joi from 'joi';
 
 const equipSchema = Joi.object({
-    cardNumber: Joi.number().integer().required(),
-    lineUp: Joi.string().valid("A", "B").required(),
-    position: Joi.string().valid("forward", "midfielder", "defender", "manager").required(),
+  cardNumber: Joi.number().integer().required(),
+  lineUp: Joi.string().valid('A', 'B').required(),
+  position: Joi.string().valid('forward', 'midfielder', 'defender', 'manager').required(),
 }).unknown(true);
 
 const unequipSchema = Joi.object({
-    cardNumber: Joi.number().integer().required(),
-    lineUp: Joi.string().valid("A", "B").required(),
-    position: Joi.string().valid("forward", "midfielder", "defender", "manager").required(),
+  cardNumber: Joi.number().integer().required(),
+  lineUp: Joi.string().valid('A', 'B').required(),
+  position: Joi.string().valid('forward', 'midfielder', 'defender', 'manager').required(),
 }).unknown(true);
 
 const formationsValidationJoi = {
-    equipCodeBodyValidation: async (req, res, next) => {
-        const validation = await equipSchema.validateAsync(req.body);
+  equipCodeBodyValidation: async (req, res, next) => {
+    try {
+      const validation = await equipSchema.validateAsync(req.body);
+      next();
+    } catch (error) {
+      next(error);
+    }
+  },
+  unequipCodeBodyValidation: async (req, res, next) => {
+    try {
+      const validation = await unequipSchema.validateAsync(req.body);
 
-        if (validation.error) {
-            return res.status(400).json({message: "equipCodeBodyValidation가 발생하였습니다."});
-        }
-
-        next();
-    },
-    unequipCodeBodyValidation: async (req, res, next) => {
-        const validation = await equipSchema.validateAsync(req.body);
-
-        if (validation.error) {
-            return res.status(400).json({message: "unequipCodeBodyValidation 발생하였습니다."});
-        }
-
-        next();
-    },
-}
+      next();
+    } catch (error) {
+      next(error);
+    }
+  },
+};
 
 export default formationsValidationJoi;
