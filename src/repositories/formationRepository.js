49044b4eb236a4a982f6657aec3378f0302428d7
YongHyeon1231 <120100530+YongHyeon1231@users.noreply.github.com>
2024-09-23 21:01:28 +0900

    포메이션 부분 리펙토링 - Repository Pattern - 고치는중
    아직 하는중

---
+++ src/repositories/formationRepository.js
@@ -0,0 +1,62 @@
+import { prisma } from '../lib/utils/prisma/index.js';
+
+class FormationRepository {
+    async findFormation(userId, clubId, lineUp, position) {
+        return await prisma.formations.findFirst({
+            where: {
+                lineUp,
+                position,
+                userId,
+                clubId,
+            }
+        });
+    }
+
+    async findAnyFormation(userId, clubId, lineUp) {
+        return await prisma.formations.findFirst({
+            where: {
+                userId,
+                clubId,
+                lineUp
+            }
+        })
+    }
+
+    async createFormation(data) {
+        return await prisma.formations.create(data);
+    }
+
+    async updateFormation(formationId, data, userId, clubId) {
+        return await prisma.formations.update({
+            where : {
+                formationId,
+                userId,
+                clubId,
+            },
+            data
+        });
+    }
+
+    async updateTeamTotalStat(lineUp, userId, clubId, teamTotalStat) {
+        return await prisma.formations.updateMany({
+            whgere: {
+                userId,
+                clubId,
+                lineUp,
+            },
+            data: { teamTotalStat: teamTotalStat },
+        });
+    }
+
+    async deleteFormation(formationId, userId, clubId) {
+        return await prisma.formations.delete({
+            where: {
+                formationId,
+                userId,
+                clubId,
+            },
+        });
+    }
+}
+
+export default new FormationRepository();
